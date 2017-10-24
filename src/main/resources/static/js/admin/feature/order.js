fd650f6a13f4c363868226d2c510ab1870261ea2
mbstu <mbstu.shahin@gmail.com>
2017-10-25 00:12:42 +0600

    Product add to cart done

--- src/main/resources/static/js/admin/feature/order.js
+++ src/main/resources/static/js/admin/feature/order.js
@@ -16,17 +16,53 @@ app.tab.order = function() {
         var _self = this;
         var productSelection = panel.find(".product-selection");
         var quantity = productSelection.find(".quantity");
-        var price = productSelection.find(".unit-price");
+        var unitPrice = productSelection.find(".unit-price");
         var priceTotal = productSelection.find(".total-price");
         productSelection.find("select.product").change(function (evt) {
             var option = $(this).find(":selected");
             var item = option.data();
             quantity.val(1);
-            price.val(item.basePrice);
+            unitPrice.val(item.basePrice);
             priceTotal.val(item.basePrice * 1);
         }).trigger("change");
-        quantity.add(price).change(function () {
-            priceTotal.val(price.val() * quantity.val());
+        quantity.add(unitPrice).change(function () {
+            priceTotal.val(unitPrice.val() * quantity.val());
+        })
+
+        productSelection.find(".add-to-cart").click(function () {
+            var select = productSelection.find("select.product")
+            var option = select.find(":selected");
+            var data = option.data();
+            var row = panel.find(".item-template").clone().show().removeClass("item-template");
+            row.find("input").prop("disabled", false);
+
+            row.find(".id .value").text(select.val());
+            row.find(".id input").val(select.val());
+
+            row.find(".item .value").text(option.text());
+            row.find(".item input").val(option.text());
+
+            row.find(".unit-price input").val(unitPrice.val());
+            row.find(".quantity input").val(quantity.val());
+
+
+            row.find(".total-price .value").text(priceTotal.val());
+            row.find(".total-price .cost-price").val(data.costPrice);
+            row.find(".total-price .total").val(priceTotal.val());
+
+            panel.find(".cart-table tbody").append(row);
+        });
+
+        panel.on("change", ".order-item .unit-price input, .order-item .quantity input", function () {
+            var row = $(this).parents(".order-item");
+            var priceBlock = row.find(".total-price");
+            var price = row.find(".unit-price input").val() * row.find(".quantity input").val();
+            priceBlock.find(".value").text(price);
+            priceBlock.find(".total").val(price);
+        })
+
+        panel.on("click", ".action-navigator .remove", function () {
+            $(this).parents(".order-item").remove();
         })
     }
 
